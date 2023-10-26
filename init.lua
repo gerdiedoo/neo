fa9ffb0bec357caee847e1fcbd32b77940b08a38
gerdiedoo <gerd14435296@gmail.com>
2023-10-26 13:38:47 +0800

    file explorer/nvim-tree

--- init.lua
+++ init.lua
@@ -1,18 +1,22 @@
 -- config by gerdlowelljana
+--vim.g.loaded_netrw = 1
+--vim.g.loaded_netrwPlugin = 1
 
 -- bootstrapped lazy vim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+     vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+     })
 end
 vim.opt.rtp:prepend(lazypath)
+
+--require("nvim-tree").setup()
 
 -- some basic configurationsa
 --require('settings')
@@ -152,6 +156,23 @@ require('lazy').setup({
             },
         },
     },
+    {
+        "nvim-treesitter/nvim-treesitter", 
+        build = ":TSUpdate"
+    },
+    {
+        import = "nvim-tree"
+    },
+    {
+        'akinsho/toggleterm.nvim', 
+        version = "*", 
+        config = true
+    },
+    --{'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
+    --{
+    --    'nvim-tree/nvim.lua',
+    --    lazy = true,
+    --},   
 })
 
 -- some basic configurationsa
@@ -185,3 +206,14 @@ require("gruvbox").setup({
     transparent_mode = false,
 })
 vim.cmd("colorscheme gruvbox")
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
