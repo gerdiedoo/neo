cde784c96c994fad4426e19d76248acacb8692ea
gerdiedoo <gerd14435296@gmail.com>
2023-11-17 23:20:25 +0800

    keybind kj for esc

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -48,7 +48,7 @@ vim.o.tabstop=4
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
+vim.keymap.set('i', 'kj', '<Esc>',{ noremap = true, silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
